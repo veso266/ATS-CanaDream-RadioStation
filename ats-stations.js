a8e84b46f73cf1a6cf0274fe0dbd5e2c75502eeb
veso266 <veso266@gmail.com>
2017-11-23 19:32:24 +0100

    Update ats-stations.js

--- ats-stations.js
+++ ats-stations.js
@@ -1,4 +1,41 @@
 var stations_custom = {
+    "vancouver": [
+        {
+            name: "CBC Radio One - CBC Vancouver",
+            logo: "http://www.cbc.ca/mediacentre/content/images/620x296-CBC_RadioOne.jpg",
+            url: "http://www.cbc.ca/listen/live/radio1/vancouver"
+        },
+        {
+            name: "CBC Radio 2",
+            logo: "http://www.cbc.ca/mediacentre/content/images/Radio2_Logo_highres.png",
+            url: "http://www.cbc.ca/listen/live/radio2/vancouver"
+        },
+        {
+            name: "AM 730",
+            logo: "http://img.corusradio.com/fb-like-icons/1500x1500/chmjam.jpg",
+            url: "http://live.leanstream.co/CHMJAM-MP3?type=.mp3&playertype=dplayer"
+        },
+        {
+            name: "TSN 1040",
+            logo: "https://pbs.twimg.com/profile_images/620989406116204544/i2AkMJm__400x400.jpg",
+            url: "http://playerservices.streamtheworld.com/api/livestream-redirect/CKSTAMAAC.aac"
+        },
+        {
+            name: "Spice Radio",
+            logo: "images/vancouver/Spice Radio.png",
+            url: "http://d2e1wsg2gpoq0n.cloudfront.net/livecf/CJRJ/playlist.m3u8"
+        },
+        {
+            name: "Ici Musique Vancouver",
+            logo: "images/vancouver/Ici Musique Vancouver.png",
+            url: "http://7bvan0.akacast.akamaistream.net/7/286/177417/v1/rc.akacast.akamaistream.net/7BVAN0"
+        },
+        {
+            name: "93.7 JRfm",
+            logo: "images/vancouver/93.7 JRfm.png",
+            url: "http://7bvan0.akacast.akamaistream.net/7/286/177417/v1/rc.akacast.akamaistream.net/7BVAN0"
+        },
+    ],
     "yukon": [
         {
             name: "CBC Radio One - CBC North",
