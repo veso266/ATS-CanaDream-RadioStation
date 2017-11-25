d486fc4aec752273559e45a12ffcecb5f9b01342
veso266 <veso266@gmail.com>
2017-11-25 13:58:58 +0100

    added cities definitions

---
+++ cities-canada.js
@@ -0,0 +1,63 @@
+var country_properties_canada {
+	"vancouver": {
+        name: "Vancouver",
+        name_english: "Vancouver",
+        code: "ca-va",
+        relative_radius: 1.2
+	},
+	"kamloops": {
+        name: "Kamloops",
+        name_english: "Kamloops",
+        code: "ca-ka",
+        relative_radius: 1.2
+	},
+	"princegeorge": {
+        name: "Prince George",
+        name_english: "Prince George",
+        code: "ca-pg",
+        relative_radius: 1.2
+	},
+
+};
+
+var city_properties_canada = {
+	"vancouver": {
+        relative_whitenoise: 0.95
+    },
+	"kamloops": {
+        relative_whitenoise: 0.95
+    },
+	"princegeorge": {
+        relative_whitenoise: 0.95
+    },
+//CANADA
+};
+
+
+var cities_mexico = [
+	{
+      "gameName": "vancouver",
+      "realName": "Vancouver",
+      "country": "britcolumb",
+      "x": "-98879.3",
+      "y": "122.016",
+      "z": "-73589.9"
+    },
+	{
+      "gameName": "kamloops",
+      "realName": "Kamloops",
+      "country": "britcolumb",
+      "x": "-86562.3",
+      "y": "111.246",
+      "z": "-79463.3"
+    },
+	{
+      "gameName": "princegeorge",
+      "realName": "Prince George",
+      "country": "britcolumb",
+      "x": "-87517.5",
+      "y": "85.8359",
+      "z": "-99375.6"
+    },
+	
+];
